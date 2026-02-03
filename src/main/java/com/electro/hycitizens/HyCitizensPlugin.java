1cc6845bcd7109063411ddd4d5035bff278761ad
Electro Games <2cool4u567@gmail.com>
2026-02-03 04:51:12 -0600

    Updated to v1.3.2

--- src/main/java/com/electro/hycitizens/HyCitizensPlugin.java
+++ src/main/java/com/electro/hycitizens/HyCitizensPlugin.java
@@ -4,17 +4,23 @@ import com.electro.hycitizens.actions.BuilderActionInteract;
 import com.electro.hycitizens.commands.CitizensCommand;
 import com.electro.hycitizens.listeners.*;
 import com.electro.hycitizens.managers.CitizensManager;
+import com.electro.hycitizens.models.CitizenData;
 import com.electro.hycitizens.ui.CitizensUI;
 import com.electro.hycitizens.util.ConfigManager;
+import com.electro.hycitizens.util.UpdateChecker;
 import com.hypixel.hytale.event.EventPriority;
+import com.hypixel.hytale.server.core.event.events.ShutdownEvent;
 import com.hypixel.hytale.server.core.event.events.player.*;
 import com.hypixel.hytale.server.core.plugin.JavaPlugin;
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
 import com.hypixel.hytale.server.core.universe.world.events.ChunkPreLoadProcessEvent;
+import com.hypixel.hytale.server.core.universe.world.events.ecs.ChunkUnloadEvent;
 import com.hypixel.hytale.server.npc.NPCPlugin;
 
 import javax.annotation.Nonnull;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HyCitizensPlugin extends JavaPlugin {
     private static HyCitizensPlugin instance;
@@ -25,6 +31,7 @@ public class HyCitizensPlugin extends JavaPlugin {
     // Listeners
     private PlayerAddToWorldListener addToWorldListener;
     private ChunkPreLoadListener chunkPreLoadListener;
+    private ChunkUnloadListener chunkUnloadListener;
     private PlayerConnectionListener connectionListener;
 
     public HyCitizensPlugin(@Nonnull JavaPluginInit init) {
@@ -45,6 +52,7 @@ public class HyCitizensPlugin extends JavaPlugin {
         // Initialize listeners
         this.addToWorldListener = new PlayerAddToWorldListener(this);
         this.chunkPreLoadListener = new ChunkPreLoadListener(this);
+        this.chunkUnloadListener = new ChunkUnloadListener(this);
         this.connectionListener = new PlayerConnectionListener(this);
 
         NPCPlugin.get().registerCoreComponentType("CitizenInteraction", BuilderActionInteract::new);
@@ -55,7 +63,7 @@ public class HyCitizensPlugin extends JavaPlugin {
 
     @Override
     protected void start() {
-
+        UpdateChecker.checkAsync();
     }
 
     @Override
@@ -67,9 +75,29 @@ public class HyCitizensPlugin extends JavaPlugin {
 
     private void registerEventListeners() {
         getEventRegistry().register(PlayerDisconnectEvent.class, connectionListener::onPlayerDisconnect);
+        getEventRegistry().register(PlayerConnectEvent.class, connectionListener::onPlayerConnect);
+
         this.getEntityStoreRegistry().registerSystem(new EntityDamageListener(this));
         //getEventRegistry().registerGlobal(AddPlayerToWorldEvent.class, addToWorldListener::onAddPlayerToWorld);
         getEventRegistry().registerGlobal(EventPriority.LAST, ChunkPreLoadProcessEvent.class, chunkPreLoadListener::onChunkPreload);
+
+        // We need to despawn citizens with player skins to prevent issues
+        getEventRegistry().register((short) -40, ShutdownEvent.class, event -> {
+            List<CitizenData> citizens = getCitizensManager().getAllCitizens();
+            for (CitizenData citizen : citizens) {
+                if (citizen.isPlayerModel()) {
+                    try {
+                        getCitizensManager().despawnCitizenNPC(citizen);
+                    } catch (Exception e) {
+                        getLogger().atSevere().withCause(e).log("Failed to despawn citizen: " + citizen.getId());
+                    }
+                }
+            }
+        });
+
+
+        // Does not work, crashes
+        //getChunkStoreRegistry().registerSystem(new ChunkUnloadListener(this));
     }
 
     public static HyCitizensPlugin get() {
